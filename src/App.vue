867669bea5c280559d1869eb02f030038347cc22
guosongqing <1084737312@qq.com>
2022-09-13 15:57:15 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HelloWorldVue from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorldVue />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <div class="left">left</div>
+    <div class="center"></div>
+    <div class="right">right</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  // https://blog.csdn.net/weixin_41487448/article/details/92830761
+  height: inherit;
+  display: flex;
+  
+  .left {
+    width: 100px;
+    background-color: red;
+  }
+
+  .center {
+    flex: 1;
+    background-color: yellowgreen;
+  }
+
+  .right {
+    width: 100px;
+    background-color: red;
+  }
+}
+</style>
